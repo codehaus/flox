90872bb14a11ec29d58e9ce9a1e87203fad23907
bwalding <bwalding@b0fb54b5-402a-0410-a5c0-f71c8158f580>
2005-04-30 05:56:54 +0000

    more usuable toString for debugging

--- flox-engine/src/main/flox/def/State.java
+++ flox-engine/src/main/flox/def/State.java
@@ -14,16 +14,19 @@ import java.util.*;
  */
 public class State implements Serializable
 {
+
     private Process process;
+
     private String name;
+
     private List<Transition> transitions;
+
     private Action action;
 
-    protected State(Process process,
-                    String name)
+    protected State( Process process, String name )
     {
-        this.process     = process;
-        this.name        = name;
+        this.process = process;
+        this.name = name;
         this.transitions = new ArrayList<Transition>();
     }
 
@@ -36,10 +39,9 @@ public class State implements Serializable
     {
         return name;
     }
-    
-    public Transition addTransition(String name,
-                                    State destination)
-        throws DuplicateTransitionException, ProcessMismatchException
+
+    public Transition addTransition( String name, State destination ) throws DuplicateTransitionException,
+            ProcessMismatchException
     {
         for ( Iterator<Transition> transitionIter = transitions.iterator(); transitionIter.hasNext(); )
         {
@@ -47,22 +49,16 @@ public class State implements Serializable
 
             if ( transition.getName().equals( name ) )
             {
-                throw new DuplicateTransitionException( this,
-                                                        name,
-                                                        destination );
+                throw new DuplicateTransitionException( this, name, destination );
             }
         }
 
-
         if ( destination.getProcess() != getProcess() )
         {
-            throw new ProcessMismatchException( this,
-                                                destination );
+            throw new ProcessMismatchException( this, destination );
         }
 
-        Transition transition = new Transition( name,
-                                                this,
-                                                destination );
+        Transition transition = new Transition( name, this, destination );
 
         this.transitions.add( transition );
 
@@ -73,19 +69,19 @@ public class State implements Serializable
     {
         return Collections.unmodifiableList( this.transitions );
     }
-    
-    public Transition getTransition(String name)
+
+    public Transition getTransition( String name )
     {
-        for (Iterator transIter = this.transitions.iterator(); transIter.hasNext(); )
+        for ( Iterator transIter = this.transitions.iterator(); transIter.hasNext(); )
         {
-            Transition trans = (Transition) transIter.next();
-            
+            Transition trans = ( Transition ) transIter.next();
+
             if ( trans.getName().equals( name ) )
             {
                 return trans;
             }
         }
-        
+
         return null;
     }
 
@@ -94,9 +90,14 @@ public class State implements Serializable
         return action;
     }
 
-    public void setAction(Action action)
+    public void setAction( Action action )
     {
         this.action = action;
+    }
+
+    public String toString()
+    {
+        return getClass().getName() + "[name=" + getName() + "]";
     }
 
 }
