c27d63b81f6303931e201858492efc930d8d1287
bob <bob@b0fb54b5-402a-0410-a5c0-f71c8158f580>
2005-07-17 07:06:22 +0000

    customizing the display for start/end, etc

--- flox-visual/src/main/flox/visual/JpegWriter.java
+++ flox-visual/src/main/flox/visual/JpegWriter.java
@@ -2,8 +2,12 @@ package flox.visual;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Rectangle;
+import java.awt.Shape;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -17,10 +21,15 @@ import com.sun.image.codec.jpeg.JPEGImageEncoder;
 
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.Vertex;
+import edu.uci.ics.jung.graph.decorators.VertexFontFunction;
+import edu.uci.ics.jung.graph.decorators.VertexPaintFunction;
+import edu.uci.ics.jung.graph.decorators.VertexShapeFunction;
+import edu.uci.ics.jung.graph.decorators.VertexStringer;
 import edu.uci.ics.jung.graph.impl.DirectedSparseEdge;
 import edu.uci.ics.jung.graph.impl.DirectedSparseGraph;
 import edu.uci.ics.jung.graph.impl.SimpleDirectedSparseVertex;
 import edu.uci.ics.jung.utils.UserDataContainer;
+import edu.uci.ics.jung.visualization.Coordinates;
 import edu.uci.ics.jung.visualization.Layout;
 import edu.uci.ics.jung.visualization.PluggableRenderer;
 import edu.uci.ics.jung.visualization.Renderer;
@@ -31,8 +40,9 @@ import flox.def.State;
 import flox.def.Transition;
 
 
-public class JpegWriter
+public class JpegWriter implements VertexStringer, VertexFontFunction, VertexShapeFunction, VertexPaintFunction
 {
+    private Dimension size;
     private Process process;
     
     public JpegWriter()
@@ -62,7 +72,7 @@ public class JpegWriter
         {
             SimpleDirectedSparseVertex vertex = new SimpleDirectedSparseVertex();
             
-            vertex.setUserDatum( "name", state.getName(), new UserDataContainer.CopyAction.Shared() );
+            vertex.setUserDatum( FloxLayout.STATE, state, new UserDataContainer.CopyAction.Shared() );
             graph.addVertex( vertex );
             vertexIndex.put( state, vertex );
         }
@@ -77,22 +87,23 @@ public class JpegWriter
                 Vertex destVertex = vertexIndex.get( destState );
 
                 DirectedSparseEdge edge = new DirectedSparseEdge( vertex, destVertex );
-                edge.setUserDatum( "name", transition.getName(), new UserDataContainer.CopyAction.Shared() );
                 graph.addEdge( edge );
             }
         }
         
         graph.setUserDatum( FloxLayout.PROCESS, process, new UserDataContainer.CopyAction.Shared() );
         
-        Dimension vd = new Dimension( 400, 400 );
+        FloxLayout layout = new FloxLayout( graph );
         
-        CircleLayout layout = new CircleLayout( graph );
-        
-        layout.initialize( vd );
+        layout.initialize( size );
         
         PluggableRenderer renderer = new PluggableRenderer(); 
         
-        //renderer.setVertexStringer( new FloxStringer() );
+        renderer.setVertexStringer( this );
+        renderer.setVertexFontFunction( this );
+        renderer.setVertexShapeFunction( this );
+        renderer.setVertexLabelCentering( true );
+        renderer.setVertexPaintFunction( this );
         //renderer.setEdgeStringer( new FloxStringer() );
         
         FloxVisualizationViewer viewer = new FloxVisualizationViewer( layout, renderer );
@@ -100,34 +111,78 @@ public class JpegWriter
         viewer.setBackground( Color.white );
         viewer.setForeground( Color.black );
         
-        //viewer.setPreferredSize( vd );
-        //viewer.resize( vd );
-        viewer.resize( vd );
-        
-        //layout.update();
-        
-        for ( int i = 0 ; i < 100 ; ++i )
-        {
-            layout.advancePositions();
-        }
-        
+        viewer.resize( size );
         
         Dimension ld = layout.getCurrentSize();
-        viewer.setScale( (float) vd.width/ld.width, (float) vd.height/ld.height );
+        viewer.setScale( (float) size.width/ld.width, (float) size.height/ld.height );
         
-        BufferedImage image = new BufferedImage( vd.width, vd.height, BufferedImage.TYPE_INT_RGB );
+        BufferedImage image = new BufferedImage( size.width, size.height, BufferedImage.TYPE_INT_RGB );
 
         Graphics2D g2 = image.createGraphics();
+        
         g2.setBackground( Color.white );
         g2.setColor( Color.black );
         
         viewer.paintComponent( g2 );
         
-        //output.setContentType( "image/jpeg" );
-
         JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder( output );
         encoder.setJPEGEncodeParam( encoder.getDefaultJPEGEncodeParam( image ) );
         encoder.encode( image );
+    }
+
+    public void setSize( int x, int y )
+    {
+        this.size = new Dimension( x, y );
+    }
+    
+    public Dimension getSize()
+    {
+        return size;
+    }
+    
+    public String getLabel(Vertex vertex)
+    {
+        State state = (State) vertex.getUserDatum( FloxLayout.STATE );
+        
+        return state.getName();
+    }
+    
+    public Font getFont(Vertex vertex)
+    {
+        return new Font( "SansSerif", Font.BOLD, 9 );
+    }
+    
+    public Shape getShape(Vertex vertex)
+    {
+        int width = 50;
+        int height = 15;
+        
+        double x = ((Coordinates)vertex.getUserDatum( FloxLayout.COORDS )).getX();
+        double y = ((Coordinates)vertex.getUserDatum( FloxLayout.COORDS )).getY();
+        
+        return new Rectangle( 0-(width/2), 0-(height/2), width, height );
+    }
+    
+    public Paint getDrawPaint(Vertex vertex)
+    {
+        return Color.black;
+    }
+    
+    public Paint getFillPaint(Vertex vertex)
+    {
+        State state = (State) vertex.getUserDatum( FloxLayout.STATE );
+        Process process = ( Process ) vertex.getGraph().getUserDatum( FloxLayout.PROCESS );
+        
+        if ( state == process.getStartState() )
+        {
+            return new Color( 0.0f, 0.7f, 0.0f );
+        }
+        else if ( vertex.outDegree() == 0 )
+        {
+            return new Color( 0.7f, 0.0f, 0.0f );
+        }
+        
+        return new Color( 0.9f, 0.9f, 0.9f );
     }
 }
 
